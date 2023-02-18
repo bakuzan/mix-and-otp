0e1d386f140b0de1c32bd1aef722f8124b767eee
bakuzan <imannoyed4days@hotmail.co.uk>
2023-02-18 19:48:05 +0000

    Supervisor and ETS chapters

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,7 +1,16 @@
 defmodule KV do
+  use Application
+
   @moduledoc """
   Documentation for `KV`.
   """
+
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
 
   @doc """
   Hello world.
